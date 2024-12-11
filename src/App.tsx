e1e843dee607f8473504e3dc1d622c29c66a5900
Lukas Kreibig <lukaskreibig@MBP-von-Lukas-2.fritz.box>
2024-12-11 16:59:55 +0100

    change css

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ const App: React.FC = () => {
       {/* Parent container with relative positioning */}
       <div style={{ position: "relative" }}>
         {/* Dropdown components positioned absolutely */}
-        <div style={{ position: "absolute", top: "10px", left: (showSidebar === true ? "320px" : "40px"), zIndex: 1 }}>
+        <div style={{ position: "absolute", top: "10px", left: (showSidebar === true ? "300px" : "100px"), zIndex: 1 }}>
           <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
             <Dropdown
               handleSelect={handleSelect}
