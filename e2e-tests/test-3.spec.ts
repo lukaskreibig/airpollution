c4019dbf9b377e77f8d82990dd4d957910c2a582
Lukas Kreibig <lukatze@posteo.de>
2025-01-16 16:27:01 +0100

    change to headless playwright

--- e2e-tests/test-3.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.locator('[data-test-id="button-skip"]').click();
-  await page.getByLabel('Country').click();
-  await page.getByRole('option', { name: 'France' }).click();
-});
